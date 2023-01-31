e24981c2fe39fce7c0cab27891c056f5f38d1e55
TripleNine666 <kolumb1709@gmail.com>
2023-01-31 19:16:50 +0300

    First commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone: {{ friend.phone }}</strong>
+      </li>
+      <li>
+        <strong>Email: {{ friend.email }}</strong>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Stepa",
+        name: "Степа Постухов",
+        phone: "+375 (44) 123-45-67",
+        email: "stepa@gmail.com ",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
