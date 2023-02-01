8b91703617f515ee5208277e560c093d7e5ef74c
TripleNine666 <kolumb1709@gmail.com>
2023-02-01 11:54:34 +0300

    add Friend component

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Имя</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label>Телефон</label>
+      <input type="tel" v-model="phone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" />
+    </div>
+    <div>
+      <button>Добавить</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-friend"],
+  data() {
+    return {
+      id: "",
+      name: "",
+      phone: "",
+      email: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-friend", this.name, this.phone, this.email);
+    },
+  },
+};
+</script>
